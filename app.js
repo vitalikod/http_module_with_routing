19dc63cf25f2faf88b35ff69e3f09fb9db296e7e
vitalikod <vitalikod879@gmail.com>
2022-08-01 15:49:10 +0300

    007

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+// const http =require('http');
+// http.createServer((req, res) => {
+//     console.log('request!!!', res.url);
+//     res.setHeader('x-test123', 'lalala');
+//     res.setHeader('content-type', 'text/html');
+//     if (req.url === '/') {
+//         res.end('hello')
+//     } else if (req.url === '/123') {
+//         res.end('welcome')
+//     } else {
+//         res.statusCode = 404;
+//         res.end('page not found');
+//     }
+// }).listen(6000);
+
